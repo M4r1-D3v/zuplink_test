44f99c9f8dc0f40990979408e86808f21f080e08
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-21 14:40:45 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/gerenciadorDePostagem/GerenciadorDePostagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciadorDePostagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePostagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePostagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/gerenciadorDePostagem/GerenciadorDePostagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciadorDePostagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePostagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
