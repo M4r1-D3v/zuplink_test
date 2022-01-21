cae6e6ec40527c184c81bfa6766eb8771cb0e3e6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-21 15:08:41 -0300

    Criar repository postagem

---
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciadorDePostagem.postagem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostagemRepository extends CrudRepository<Postagem,Integer> {
+
+}
