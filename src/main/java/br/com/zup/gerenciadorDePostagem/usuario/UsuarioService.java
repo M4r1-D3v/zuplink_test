212c5c00b87df3b90fbfceedb51b456cdee70794
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-24 15:40:32 -0300

    Criar cadastro para usuario

--- src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioService.java
@@ -1,12 +1,23 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
+import br.com.zup.gerenciadorDePostagem.exceptions.EmailJaCadstradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario cadastrarUsuario (Usuario usuario){
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+        if (usuarioExistente.isPresent()){
+            throw new EmailJaCadstradoException();
+        }
+        return usuarioRepository.save(usuario);
+    }
 
 }
