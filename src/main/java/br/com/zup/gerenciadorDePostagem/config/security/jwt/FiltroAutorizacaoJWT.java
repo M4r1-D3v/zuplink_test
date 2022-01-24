a6902ddf36f84e354dd531c97d8c8c9615ca8c96
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-24 14:41:03 -0300

    Adicionar método pegar autenticação

--- src/main/java/br/com/zup/gerenciadorDePostagem/config/security/jwt/FiltroAutorizacaoJWT.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/config/security/jwt/FiltroAutorizacaoJWT.java
@@ -1,7 +1,10 @@
 package br.com.zup.gerenciadorDePostagem.config.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -17,6 +20,17 @@ public class FiltroAutorizacaoJWT extends BasicAuthenticationFilter {
         super(authenticationManager);
         this.jwtComponent = jwtComponent;
         this.userDetailsService = userDetailsService;
+    }
+
+    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token){
+        if (!jwtComponent.tokenValido(token)){
+            throw new TokenInvalidoException();
+        }
+
+        Claims claims = jwtComponent.pegarClaims(token);
+        UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(usuarioLogado,null,usuarioLogado.getAuthorities());
     }
 
 }
