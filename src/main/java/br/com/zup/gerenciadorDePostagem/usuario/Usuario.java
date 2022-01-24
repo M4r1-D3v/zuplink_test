397c1b60f0884f11af4751868420a03dc9b7ba7b
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-24 10:45:23 -0300

    Modelar entidade Usuario

--- src/main/java/br/com/zup/gerenciadorDePostagem/usuario/Usuario.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/Usuario.java
@@ -1,7 +1,33 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
-import javax.persistence.Entity;
+import br.com.zup.gerenciadorDePostagem.postagem.Postagem;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Table(name = "Usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+    @Column(nullable = false)
+    private List<Postagem> postagem;
+
 }
+
+
+
