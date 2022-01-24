87d4818d1c08e5dc18ddfabafbb18da52e93d7e1
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-24 10:34:37 -0300

    Sobreescrever método configure, desabilitando o csrf e adicionando as rotas abertas

--- src/main/java/br/com/zup/gerenciadorDePostagem/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/config/security/ConfiguracaoDeSeguranca.java
@@ -1,6 +1,8 @@
 package br.com.zup.gerenciadorDePostagem.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +10,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca  extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/usuario").permitAll()
+                .antMatchers(HttpMethod.GET,"/postagem").permitAll()
+                .anyRequest().authenticated();
+
+    }
 }
