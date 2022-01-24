b31afe47f985c5c84e8627449e1d9d51ef9f824d
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-24 18:47:31 -0300

    Criar Classe para saida DTO

--- src/main/java/br/com/zup/gerenciadorDePostagem/usuario/dtos/UsuarioSaidaDTO.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/dtos/UsuarioSaidaDTO.java
@@ -1,2 +1,22 @@
-package br.com.zup.gerenciadorDePostagem.usuario.dtos;public class UsuarioSaidaDTO {
+package br.com.zup.gerenciadorDePostagem.usuario.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class UsuarioSaidaDTO {
+
+    @Id
+    private String id;
+    private String email;
+    private String senha;
+
 }
