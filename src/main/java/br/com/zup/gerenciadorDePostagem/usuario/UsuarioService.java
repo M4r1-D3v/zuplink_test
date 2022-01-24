93c2f8cccd95e535bd514ee0dc16ccbf0b4408a9
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-24 18:42:20 -0300

    Criar método para atualizar usuario

--- src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioService.java
@@ -1,6 +1,8 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
 import br.com.zup.gerenciadorDePostagem.exceptions.EmailJaCadstradoException;
+import br.com.zup.gerenciadorDePostagem.exceptions.UsuarioNaoCadastrado;
+import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,20 @@ public class UsuarioService {
             throw new EmailJaCadstradoException();
         }
         return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizarUsuario(String id, UsuarioDto usuarioDto){
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if(usuarioOptional.isEmpty()){
+            throw new UsuarioNaoCadastrado ("O usuário não existe, favor Cadastrar");
+        }
+        Usuario usuarioParaAtualizar = usuarioOptional.get();
+        usuarioParaAtualizar.setEmail(usuarioDto.getEmail());
+
+        usuarioRepository.save(usuarioParaAtualizar);
+
+
+        return usuarioParaAtualizar;
     }
 
 }
