3365385bfca7c865ebc51d4b5ecff4f25d2ff114
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-24 15:42:58 -0300

    Implementar o cadastro no controller

--- src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioController.java
@@ -1,8 +1,12 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
+import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/usuario")
@@ -10,5 +14,15 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarUsuario(@RequestBody @Valid UsuarioDto usuarioDTO) {
+        Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
+        usuarioService.cadastrarUsuario(usuario);
+    }
 
 }
