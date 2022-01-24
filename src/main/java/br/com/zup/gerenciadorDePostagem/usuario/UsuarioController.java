0b401121ba69db00d24689e500b1e3fb55235073
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-24 18:48:26 -0300

    Implementar Atualização do usuario

--- src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
 import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioDto;
+import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +24,12 @@ public class UsuarioController {
     public void cadastrarUsuario(@RequestBody @Valid UsuarioDto usuarioDTO) {
         Usuario usuario = modelMapper.map(usuarioDTO, Usuario.class);
         usuarioService.cadastrarUsuario(usuario);
+    }
+
+    public UsuarioSaidaDTO atualizarUsuario (@PathVariable String id, @PathVariable @Valid UsuarioDto usuarioDto){
+        Usuario usuario = usuarioService.atualizarUsuario(id, usuarioDto);
+
+        return modelMapper.map(usuario, UsuarioSaidaDTO.class);
     }
 
 }
