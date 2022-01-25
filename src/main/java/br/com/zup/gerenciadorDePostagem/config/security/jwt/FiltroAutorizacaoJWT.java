8807a855a3eff36c98ae0d42179c4abc46d5a4d5
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-25 08:15:58 -0300

    Importar classe Token invalido exception

--- src/main/java/br/com/zup/gerenciadorDePostagem/config/security/jwt/FiltroAutorizacaoJWT.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/config/security/jwt/FiltroAutorizacaoJWT.java
@@ -1,5 +1,6 @@
 package br.com.zup.gerenciadorDePostagem.config.security.jwt;
 
+import br.com.zup.gerenciadorDePostagem.config.security.jwt.exceptions.TokenInvalidoException;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,23 +24,27 @@ public class FiltroAutorizacaoJWT extends BasicAuthenticationFilter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+
     public FiltroAutorizacaoJWT(AuthenticationManager authenticationManager, JWTComponent jwtComponent,
                                 UserDetailsService userDetailsService) {
+
         super(authenticationManager);
         this.jwtComponent = jwtComponent;
         this.userDetailsService = userDetailsService;
+
     }
 
-    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token){
 
-        if (!jwtComponent.tokenValido(token)){
+    public UsernamePasswordAuthenticationToken pegarAutenticacao(String token) {
+
+        if (!jwtComponent.tokenValido(token)) {
             throw new TokenInvalidoException();
         }
 
         Claims claims = jwtComponent.pegarClaims(token);
         UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
 
-        return new UsernamePasswordAuthenticationToken(usuarioLogado,null,usuarioLogado.getAuthorities());
+        return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
     }
 
     @Override
@@ -48,16 +53,19 @@ public class FiltroAutorizacaoJWT extends BasicAuthenticationFilter {
 
         String token = request.getHeader("Authorization");
 
-        if (token != null && token.startsWith("Token ")){
+        if (token != null && token.startsWith("Token ")) {
+
             try {
                 UsernamePasswordAuthenticationToken auth = pegarAutenticacao(token.substring(6));
                 SecurityContextHolder.getContext().setAuthentication(auth);
-            }catch (TokenInvalidoException e){
+            } catch (TokenInvalidoException e) {
                 response.setStatus(HttpStatus.FORBIDDEN.value());
             }
+
         }
 
-        chain.doFilter(request,response);
+        chain.doFilter(request, response);
 
     }
+
 }
