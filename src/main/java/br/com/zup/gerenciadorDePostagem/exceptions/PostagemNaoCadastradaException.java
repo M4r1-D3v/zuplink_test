7ebe525173cabc570eb63442cc1e9fb2100bda84
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-25 16:01:01 -0300

    Criar exception Postagem não cadastrada

---
+++ src/main/java/br/com/zup/gerenciadorDePostagem/exceptions/PostagemNaoCadastradaException.java
@@ -0,0 +1,9 @@
+package br.com.zup.gerenciadorDePostagem.exceptions;
+
+public class PostagemNaoCadastradaException extends RuntimeException {
+
+    public PostagemNaoCadastradaException(String message) {
+        super(message);
+    }
+
+}
