0e034e37a82fc8e3f748a5c048b0edf7ce0c631d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-25 11:42:48 -0300

    Adicionar método para cadastrar postagem

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemController.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemController.java
@@ -1,8 +1,15 @@
 package br.com.zup.gerenciadorDePostagem.postagem;
 
+import br.com.zup.gerenciadorDePostagem.config.security.UsuarioLogado;
+import br.com.zup.gerenciadorDePostagem.postagem.dtos.CadastroPostagemDTO;
+import br.com.zup.gerenciadorDePostagem.usuario.Usuario;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/postagem")
@@ -10,5 +17,19 @@ public class PostagemController {
 
     @Autowired
     private PostagemService postagemService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarPostagem(@RequestBody @Valid CadastroPostagemDTO cadastroPostagemDTO,
+                                  Authentication authentication) {
+
+        UsuarioLogado usuarioLogado = (UsuarioLogado) authentication.getPrincipal();
+        postagemService.salvarPostagem(modelMapper.map(cadastroPostagemDTO, Postagem.class),
+                modelMapper.map(usuarioLogado, Usuario.class));
+
+    }
 
 }
