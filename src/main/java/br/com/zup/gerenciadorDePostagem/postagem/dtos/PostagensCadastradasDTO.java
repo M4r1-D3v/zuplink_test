4e415737662279a382c2bf248a5743e45cf47de1
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2022-01-25 15:24:18 -0300

    Criar dtos para metodo exibir postagens

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/dtos/PostagensCadastradasDTO.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/dtos/PostagensCadastradasDTO.java
@@ -1,2 +1,28 @@
-package br.com.zup.gerenciadorDePostagem.postagem.dtos;public class PostagensCadastradasDTO {
+package br.com.zup.gerenciadorDePostagem.postagem.dtos;
+
+
+import br.com.zup.gerenciadorDePostagem.enums.Area;
+import br.com.zup.gerenciadorDePostagem.enums.Tema;
+import br.com.zup.gerenciadorDePostagem.enums.Tipo;
+import br.com.zup.gerenciadorDePostagem.usuario.dtos.ExibirUsuarioPostagemDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PostagensCadastradasDTO {
+
+    private String titulo;
+    private String descricao;
+    private String link;
+    private Tipo tipo;
+    private Tema tema;
+    private Area areaAtuacao;
+    private ExibirUsuarioPostagemDTO usuarioPostagemDTO;
+
 }
