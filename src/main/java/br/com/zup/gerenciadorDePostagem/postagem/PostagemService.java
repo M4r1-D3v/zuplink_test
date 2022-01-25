78943d39485cd3b0299a2e902dcec6a4ecede2ca
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-25 11:39:59 -0300

    Adicionar método para salvar  postagem

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemService.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemService.java
@@ -1,12 +1,25 @@
 package br.com.zup.gerenciadorDePostagem.postagem;
 
+import br.com.zup.gerenciadorDePostagem.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class PostagemService {
 
     @Autowired
     private PostagemRepository postagemRepository;
+
+
+    public Postagem salvarPostagem(Postagem postagem, Usuario autorPostagem) {
+        postagem.setLikes(0);
+        postagem.setDeslikes(0);
+        postagem.setAutorPostagem(autorPostagem);
+        postagem.setDataDeCadastro(LocalDate.now());
+
+        return postagemRepository.save(postagem);
+    }
 
 }
