c07344829dd7661676c61da19cdcf6760af59b43
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2022-01-27 14:03:34 -0300

    Ajustar configuração pré teste

--- src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
@@ -1,12 +1,15 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
 
+import br.com.zup.gerenciadorDePostagem.config.security.UsuarioLogado;
+import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioDto;
+import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioSaidaDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -17,23 +20,40 @@ public class UsuarioControllerTest {
     @MockBean
     private UsuarioService usuarioService;
 
-   @Autowired
+
+    @Autowired
     private MockMvc mockMvc;
 
-   private ObjectMapper objectMapper;
-   private Usuario usuario;
-   private List<Usuario>usuarios;
+    private ObjectMapper objectMapper;
+    private Usuario usuario;
+    private List<Usuario> usuarios;
+    private UsuarioDto usuarioDto;
+    private UsuarioLogado usuarioLogado;
+    private Authentication authentication;
+    private UsuarioSaidaDTO usuarioSaidaDTO;
 
-   @BeforeEach
-    private void setup(){
-       objectMapper = new ObjectMapper();
+    @BeforeEach
+    private void setup() {
+        objectMapper = new ObjectMapper();
 
-       usuario = new Usuario();
-       usuario.setNome("Ana");
-       usuario.setEmail("ana@zup.com.br");
-       usuario.setSenha("123456");
-       usuario.setId("1");
+        usuario = new Usuario();
+        usuario.setNome("Ana");
+        usuario.setEmail("ana@zup.com.br");
+        usuario.setSenha("123456");
+        usuario.setId("1");
 
-       usuarios = Arrays.asList(usuario);
-   }
+        usuarios = Arrays.asList(usuario);
+
+        usuarioDto = new UsuarioDto();
+        usuarioDto.setNome("Ana");
+        usuarioDto.setEmail("ana@zup.com.br");
+        usuarioDto.setSenha("123456");
+
+        usuarioLogado = new UsuarioLogado();
+        usuarioLogado.setNome("Ana");
+        usuarioLogado.setEmail("ana@zup.com.br");
+        usuarioLogado.setSenha("123456");
+        usuarioLogado.setId("1");
+
+    }
 }
