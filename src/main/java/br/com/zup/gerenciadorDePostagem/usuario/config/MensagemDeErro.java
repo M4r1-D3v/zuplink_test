c406ee9a0b932f83368cdb2b5c3abc9a3f08e52c
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2022-01-28 12:09:20 -0300

    Criar classe mensagemDeErro

---
+++ src/main/java/br/com/zup/gerenciadorDePostagem/usuario/config/MensagemDeErro.java
@@ -0,0 +1,16 @@
+package br.com.zup.gerenciadorDePostagem.usuario.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class MensagemDeErro {
+    public String mensagem;
+    public String campo;
+
+}
