3136844e3d531c941f1feb1e44fd189819373325
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-28 17:20:41 -0300

    Testar cadastrar usuario caminho positivo

--- src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioServiceTest.java
+++ src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioServiceTest.java
@@ -29,6 +29,7 @@ class UsuarioServiceTest {
     public static final String NOME = "XABLAU";
     public static final String EMAIL = "xablau@zup.com.br";
     public static final String SENHA = "1234";
+    public static final String SENHA_CRIPTOGRAFADA = "$2a$10$/76T1TrDeFsblWwigaMkqukGYpzNk0ZbslAIaQs3NqP0Ta.fkc6lS";
     public static final String EMAIL_JA_CADASTRADO = "Email já cadastrado";
     public static final String USUARIO_NAO_CADASTRADO = "O usuário não existe, favor Cadastrar";
     public static final int INDEX = 0;
@@ -46,22 +47,34 @@ class UsuarioServiceTest {
 
 
     private Usuario usuario;
-    private Usuario usuarioTeste;
 
 
     @BeforeEach
     void setUp() {
-        usuario = new Usuario(ID_USUARIO,NOME,EMAIL,SENHA);
-        usuarioTeste = new Usuario(ID_TESTE,NOME,EMAIL,SENHA);
+        usuario = new Usuario(ID_USUARIO, NOME, EMAIL, SENHA);
     }
 
     @Test
     public void testarCadastrarUsuarioCaminhoPositivo() {
+        when(usuarioRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
+        when(encoder.encode(anyString())).thenReturn(SENHA_CRIPTOGRAFADA);
 
+        Usuario response = usuarioService.cadastrarUsuario(usuario);
+
+        assertNotNull(response);
+        assertEquals(Usuario.class, response.getClass());
+
+        assertEquals(ID_USUARIO, response.getId());
+        assertEquals(NOME, response.getNome());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(SENHA_CRIPTOGRAFADA, response.getSenha());
+
+        verify(usuarioRepository, times(1)).findByEmail(anyString());
     }
 
     @Test
-     public void testarCadastrarUsuarioExceptionEmailJaCadastrado() {
+    public void testarCadastrarUsuarioExceptionEmailJaCadastrado() {
         when(usuarioRepository.findByEmail(anyString())).thenReturn(ofNullable(usuario));
         when(usuarioRepository.save(any(Usuario.class))).thenReturn(usuario);
 
@@ -71,8 +84,8 @@ class UsuarioServiceTest {
         assertEquals(EmailJaCadastradoException.class, exception.getClass());
         assertEquals(EMAIL_JA_CADASTRADO, exception.getMessage());
 
-        verify(usuarioRepository,times(1)).findByEmail(anyString());
-        verify(usuarioRepository,times(0)).save(usuario);
+        verify(usuarioRepository, times(1)).findByEmail(anyString());
+        verify(usuarioRepository, times(0)).save(usuario);
 
     }
 
@@ -84,15 +97,15 @@ class UsuarioServiceTest {
         Usuario response = usuarioService.atualizarUsuario(ID_USUARIO, usuario);
 
         assertNotNull(response);
-        assertEquals(Usuario.class,response.getClass());
+        assertEquals(Usuario.class, response.getClass());
 
-        assertEquals(ID_USUARIO,response.getId());
-        assertEquals(NOME,response.getNome());
-        assertEquals(EMAIL,response.getEmail());
-        assertEquals(SENHA,response.getSenha());
+        assertEquals(ID_USUARIO, response.getId());
+        assertEquals(NOME, response.getNome());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(SENHA, response.getSenha());
 
-        verify(usuarioRepository,times(1)).findById(anyString());
-        verify(usuarioRepository,times(1)).save(any(Usuario.class));
+        verify(usuarioRepository, times(1)).findById(anyString());
+        verify(usuarioRepository, times(1)).save(any(Usuario.class));
 
     }
 
@@ -107,8 +120,8 @@ class UsuarioServiceTest {
         assertEquals(UsuarioNaoCadastradoException.class, exception.getClass());
         assertEquals(USUARIO_NAO_CADASTRADO, exception.getMessage());
 
-        verify(usuarioRepository,times(1)).findById(anyString());
-        verify(usuarioRepository,times(0)).save(any(Usuario.class));
+        verify(usuarioRepository, times(1)).findById(anyString());
+        verify(usuarioRepository, times(0)).save(any(Usuario.class));
 
     }
 
@@ -119,15 +132,15 @@ class UsuarioServiceTest {
         List<Usuario> response = usuarioService.exibirUsuarios();
 
         assertNotNull(response);
-        assertEquals(Usuario.class,response.get(INDEX).getClass());
+        assertEquals(Usuario.class, response.get(INDEX).getClass());
         assertEquals(1, response.size());
 
-        assertEquals(ID_USUARIO,response.get(INDEX).getId());
-        assertEquals(NOME,response.get(INDEX).getNome());
-        assertEquals(EMAIL,response.get(INDEX).getEmail());
-        assertEquals(SENHA,response.get(INDEX).getSenha());
+        assertEquals(ID_USUARIO, response.get(INDEX).getId());
+        assertEquals(NOME, response.get(INDEX).getNome());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(SENHA, response.get(INDEX).getSenha());
 
-        verify(usuarioRepository,times(1)).findAll();
+        verify(usuarioRepository, times(1)).findAll();
 
     }
 
@@ -138,10 +151,10 @@ class UsuarioServiceTest {
         RuntimeException exception = assertThrows(NaoExistemUsuariosCadastradosException.class,
                 () -> {usuarioService.exibirUsuarios();});
 
-        assertEquals(NaoExistemUsuariosCadastradosException.class,exception.getClass());
+        assertEquals(NaoExistemUsuariosCadastradosException.class, exception.getClass());
         assertEquals(NAO_HA_USUARIOS_CADASTRADOS, exception.getMessage());
 
-        verify(usuarioRepository,times(1)).findAll();
+        verify(usuarioRepository, times(1)).findAll();
 
     }
 
@@ -150,10 +163,10 @@ class UsuarioServiceTest {
         when(usuarioRepository.findByEmail(anyString())).thenReturn(Optional.ofNullable(usuario));
         doNothing().when(usuarioRepository).deleteById(anyString());
 
-        usuarioService.deletarUsuario(EMAIL,ID_USUARIO);
+        usuarioService.deletarUsuario(EMAIL, ID_USUARIO);
 
-        verify(usuarioRepository,times(1)).findByEmail(anyString());
-        verify(usuarioRepository,times(1)).deleteById(anyString());
+        verify(usuarioRepository, times(1)).findByEmail(anyString());
+        verify(usuarioRepository, times(1)).deleteById(anyString());
 
     }
 
@@ -167,8 +180,8 @@ class UsuarioServiceTest {
         assertEquals(UsuarioNaoAutorizadoException.class, exception.getClass());
         assertEquals(USUARIO_NAO_AUTORIZADO, exception.getMessage());
 
-        verify(usuarioRepository,times(1)).findByEmail(anyString());
-        verify(usuarioRepository,times(0)).deleteById(anyString());
+        verify(usuarioRepository, times(1)).findByEmail(anyString());
+        verify(usuarioRepository, times(0)).deleteById(anyString());
 
     }
 
@@ -182,8 +195,8 @@ class UsuarioServiceTest {
         assertEquals(UsuarioNaoCadastradoException.class, exception.getClass());
         assertEquals(USUARIO_NAO_ENCONTRADO, exception.getMessage());
 
-        verify(usuarioRepository,times(1)).findByEmail(anyString());
-        verify(usuarioRepository,times(0)).deleteById(anyString());
+        verify(usuarioRepository, times(1)).findByEmail(anyString());
+        verify(usuarioRepository, times(0)).deleteById(anyString());
 
     }
 
