384444ae36a3008cd0f2fa1709f08bc6de1672a3
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-31 16:39:35 -0300

    Criar filtro para Tipo

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -1,10 +1,12 @@
 package br.com.zup.gerenciadorDePostagem.postagem;
 
 import br.com.zup.gerenciadorDePostagem.enums.Area;
+import br.com.zup.gerenciadorDePostagem.enums.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface PostagemRepository extends CrudRepository<Postagem,Long> {
     List<Postagem> findAllByArea (Area area);
+    List<Postagem> findAllByTipo (Tipo tipo);
 }
