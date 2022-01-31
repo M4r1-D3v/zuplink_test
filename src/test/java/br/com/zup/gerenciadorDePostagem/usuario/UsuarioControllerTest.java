0ab2aedfbbd7767b0d5b8e47b8e400391612468c
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-01-31 08:43:00 -0300

    Refatorar classe de testes usuario controller

--- src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
@@ -1,78 +1,78 @@
 package br.com.zup.gerenciadorDePostagem.usuario;
 
 
-import br.com.zup.gerenciadorDePostagem.config.security.UsuarioLogado;
+import br.com.zup.gerenciadorDePostagem.config.security.UsuarioLoginService;
 import br.com.zup.gerenciadorDePostagem.config.security.jwt.JWTComponent;
 import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioDto;
-import br.com.zup.gerenciadorDePostagem.usuario.dtos.UsuarioSaidaDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.Arrays;
-import java.util.List;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@WebMvcTest({UsuarioController.class, ModelMapper.class, JWTComponent.class})
+@WebMvcTest({UsuarioController.class, ModelMapper.class, JWTComponent.class, UsuarioLoginService.class})
 public class UsuarioControllerTest {
+
+    public static final String NOME = "Ana";
+    public static final String EMA = "ana@zup.com.br";
+    public static final String SENHA = "1234";
+    public static final String ID_USUARIO = "1";
+
+
     @MockBean
     private UsuarioService usuarioService;
     @MockBean
     private ModelMapper modelMapper;
+    @MockBean
+    private JWTComponent jwtComponent;
+    @MockBean
+    private UsuarioLoginService usuarioLoginService;
+
     @Autowired
     private MockMvc mockMvc;
 
+
     private ObjectMapper objectMapper;
     private Usuario usuario;
-    private List<Usuario> usuarios;
     private UsuarioDto usuarioDto;
-    private UsuarioLogado usuarioLogado;
-    private Authentication authentication;
-    private UsuarioSaidaDTO usuarioSaidaDTO;
 
     @BeforeEach
     private void setup() {
         objectMapper = new ObjectMapper();
 
         usuario = new Usuario();
-        usuario.setNome("Ana");
-        usuario.setEmail("ana@zup.com.br");
-        usuario.setSenha("123456");
-        usuario.setId("1");
-
-        usuarios = Arrays.asList(usuario);
+        usuario.setNome(NOME);
+        usuario.setEmail(EMA);
+        usuario.setSenha(SENHA);
+        usuario.setId(ID_USUARIO);
 
         usuarioDto = new UsuarioDto();
-        usuarioDto.setNome("Ana");
-        usuarioDto.setEmail("ana@zup.com.br");
-        usuarioDto.setSenha("123456");
-
-        usuarioLogado = new UsuarioLogado();
-        usuarioLogado.setNome("Ana");
-        usuarioLogado.setEmail("ana@zup.com.br");
-        usuarioLogado.setSenha("123456");
-        usuarioLogado.setId("1");
+        usuarioDto.setNome(NOME);
+        usuarioDto.setEmail(EMA);
+        usuarioDto.setSenha(SENHA);
 
     }
-    @Test
-    public void testarCadastroDeUsuario()throws Exception{
-        Mockito.when(usuarioService.cadastrarUsuario(Mockito.any(Usuario.class))).thenReturn(usuario);
-        String json = objectMapper.writeValueAsString(usuario);
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/usuario")
-                .contentType(MediaType.APPLICATION_JSON).content(json))
+    @Test
+    public void testarRotaParaCadastroDeUsuarioCaminhoPositivo() throws Exception {
+        when(usuarioService.cadastrarUsuario(any(Usuario.class))).thenReturn(usuario);
+        String json = objectMapper.writeValueAsString(usuarioDto);
+
+        ResultActions resultActions = mockMvc.perform(post("/usuario")
+                        .contentType(MediaType.APPLICATION_JSON).content(json))
                 .andExpect(MockMvcResultMatchers.status().is(201)
                 );
-        Mockito.verify(usuarioService,Mockito.times(1)).cadastrarUsuario(Mockito.any());
+
+        verify(usuarioService, times(1)).cadastrarUsuario(any());
+
     }
 }
