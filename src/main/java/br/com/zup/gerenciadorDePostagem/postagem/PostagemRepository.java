59596e4e7e7820f9ad637cd47a5979cbddff0ca7
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-02-01 14:06:27 -0300

    Adicionar filtro com anotação para likes para relevancia das postagens mais curtidas

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -4,6 +4,7 @@ import br.com.zup.gerenciadorDePostagem.enums.Area;
 import br.com.zup.gerenciadorDePostagem.enums.Tema;
 import br.com.zup.gerenciadorDePostagem.enums.Tipo;
 import br.com.zup.gerenciadorDePostagem.usuario.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ public interface PostagemRepository extends CrudRepository<Postagem,Long> {
     List<Postagem> findAllByTema (Tema tema);
     List<Postagem> findAllByUsuario (Usuario autorPostagem);
     List<Postagem> findAllByLocalDate (LocalDate dataDeCadastro);
-
+    @Query(value = "SELECT * FROM postagens ORDER BY likes DESC", nativeQuery = true)
+    List<Postagem> findAllByInteger (Integer likes);
 
 }
