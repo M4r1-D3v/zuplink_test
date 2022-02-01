c6aa5fa43e11f7f7e430d356e483a6f92260230a
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-02-01 11:43:56 -0300

    Adicionar autor da postagem na repository

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -3,6 +3,7 @@ package br.com.zup.gerenciadorDePostagem.postagem;
 import br.com.zup.gerenciadorDePostagem.enums.Area;
 import br.com.zup.gerenciadorDePostagem.enums.Tema;
 import br.com.zup.gerenciadorDePostagem.enums.Tipo;
+import br.com.zup.gerenciadorDePostagem.usuario.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface PostagemRepository extends CrudRepository<Postagem,Long> {
     List<Postagem> findAllByArea (Area area);
     List<Postagem> findAllByTipo (Tipo tipo);
     List<Postagem> findAllByTema (Tema tema);
+    List<Postagem> findAllByTema (Usuario autorPostagem);
+
+
 }
