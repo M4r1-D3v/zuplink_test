347f5465786253c904fa2bae85a69eea58fb99be
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-02-01 14:38:25 -0300

    Refatorar nome

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemService.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemService.java
@@ -102,7 +102,7 @@ public class PostagemService {
             return postagemRepository.findAllByLocalDate(dataDeCadastro);
         }
         else if (likes != null){
-            return postagemRepository.findAllByInteger(likes);
+            return postagemRepository.like(likes);
         }
         else if (deslikes != null){
             return postagemRepository.deslike(deslikes);
