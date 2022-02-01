7de797ba016e111d65d6c79f0f72c3913c126588
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-02-01 07:40:16 -0300

    Testar rota para cadastrar usuario validacao nome not null

--- src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
@@ -162,4 +162,20 @@ public class UsuarioControllerTest {
 
     }
 
+    @Test
+    public void testarRotaParaCadastroDeUsuarioValidacaoNomeNotNull() throws Exception {
+        usuarioDto.setNome(null);
+        when(usuarioService.cadastrarUsuario(any())).thenReturn(usuario);
+        String json = objectMapper.writeValueAsString(usuarioDto);
+
+        ResultActions response = mockMvc.perform(post("/usuario")
+                        .contentType(APPLICATION_JSON).content(json))
+                .andExpect(status().isUnprocessableEntity());
+
+
+        assertEquals(422,response.andReturn().getResponse().getStatus());
+        verify(usuarioService, times(0)).cadastrarUsuario(any());
+
+    }
+
 }
