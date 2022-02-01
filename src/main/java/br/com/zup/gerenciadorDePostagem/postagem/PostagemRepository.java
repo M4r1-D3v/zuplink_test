5e7ca489125cc6d51b31dcfeb56354ecf7e589ac
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-02-01 11:30:58 -0300

    Implementar Tema no repository para o filtro

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -1,6 +1,7 @@
 package br.com.zup.gerenciadorDePostagem.postagem;
 
 import br.com.zup.gerenciadorDePostagem.enums.Area;
+import br.com.zup.gerenciadorDePostagem.enums.Tema;
 import br.com.zup.gerenciadorDePostagem.enums.Tipo;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 public interface PostagemRepository extends CrudRepository<Postagem,Long> {
     List<Postagem> findAllByArea (Area area);
     List<Postagem> findAllByTipo (Tipo tipo);
+    List<Postagem> findAllByTema (Tema tema);
 }
