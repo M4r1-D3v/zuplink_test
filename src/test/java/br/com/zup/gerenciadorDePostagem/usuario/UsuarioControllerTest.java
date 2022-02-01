838b6af8f31d645c1584967168bb778629c3b6b7
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-02-01 07:37:01 -0300

    Testar rota para cadastrar usuario validacao senha not blank

--- src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
+++ src/test/java/br/com/zup/gerenciadorDePostagem/usuario/UsuarioControllerTest.java
@@ -130,4 +130,20 @@ public class UsuarioControllerTest {
 
     }
 
+    @Test
+    public void testarRotaParaCadastroDeUsuarioValidacaoSenhaNotBlank() throws Exception {
+        usuarioDto.setSenha("");
+        when(usuarioService.cadastrarUsuario(any())).thenReturn(usuario);
+        String json = objectMapper.writeValueAsString(usuarioDto);
+
+        ResultActions response = mockMvc.perform(post("/usuario")
+                        .contentType(APPLICATION_JSON).content(json))
+                .andExpect(status().isUnprocessableEntity());
+
+
+        assertEquals(422,response.andReturn().getResponse().getStatus());
+        verify(usuarioService, times(0)).cadastrarUsuario(any());
+
+    }
+
 }
