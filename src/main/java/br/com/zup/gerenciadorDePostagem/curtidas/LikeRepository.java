63bdd78010219184a2294a0884d44a384a47fd87
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-02-02 13:32:02 -0300

    Adicionar query já curtiu

--- src/main/java/br/com/zup/gerenciadorDePostagem/curtidas/LikeRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/curtidas/LikeRepository.java
@@ -1,7 +1,13 @@
 package br.com.zup.gerenciadorDePostagem.curtidas;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends CrudRepository<Like, Long> {
+
+    @Query(value = "SELECT * FROM likes WHERE id_postagem=:id_postagem AND id_usuario=:id_usuario", nativeQuery = true)
+    Optional<Like> jaCurtiu(Long id_postagem, String id_usuario);
 
 }
