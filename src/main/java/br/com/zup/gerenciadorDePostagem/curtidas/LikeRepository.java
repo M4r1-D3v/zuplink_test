a3cbe62b0febddead9cdf2fa0453eb0734c7262a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-02-02 13:30:45 -0300

    Criar repository para like

---
+++ src/main/java/br/com/zup/gerenciadorDePostagem/curtidas/LikeRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.gerenciadorDePostagem.curtidas;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LikeRepository extends CrudRepository<Like, Long> {
+
+}
