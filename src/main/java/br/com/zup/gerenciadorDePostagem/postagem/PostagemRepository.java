ba58c84444fb726409c965d65fa1253e4b8f694a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-02-02 10:32:19 -0300

    Fix: adicionar querys para efetuar busca no banco de dados

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -1,25 +1,26 @@
 package br.com.zup.gerenciadorDePostagem.postagem;
 
-import br.com.zup.gerenciadorDePostagem.enums.Area;
-import br.com.zup.gerenciadorDePostagem.enums.Tema;
-import br.com.zup.gerenciadorDePostagem.enums.Tipo;
-import br.com.zup.gerenciadorDePostagem.usuario.Usuario;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface PostagemRepository extends CrudRepository<Postagem, Long> {
-    List<Postagem> findAllByArea(Area area);
 
-    List<Postagem> findAllByTipo(Tipo tipo);
+    @Query(value = "SELECT * FROM postagens WHERE area_atuacao=:area", nativeQuery = true)
+    List<Postagem> area(String area);
 
-    List<Postagem> findAllByTema(Tema tema);
+    @Query(value = "SELECT * FROM postagens WHERE tipo=:tipo", nativeQuery = true)
+    List<Postagem> tipo(String tipo);
 
-    List<Postagem> findAllByUsuario(Usuario autorPostagem);
+    @Query(value = "SELECT * FROM postagens WHERE tema=:tema", nativeQuery = true)
+    List<Postagem> tema(String tema);
 
-    List<Postagem> findAllByLocalDate(LocalDate dataDeCadastro);
+    @Query(value = "SELECT * FROM postagens WHERE autor_postagem_id=:autorPostagem", nativeQuery = true)
+    List<Postagem> autorPostagem(String autorPostagem);
+
+    @Query(value = "SELECT * FROM postagens WHERE data_de_cadastro=:dataDeCadastro", nativeQuery = true)
+    List<Postagem> dataDeCadastro(String dataDeCadastro);
 
     @Query(value = "SELECT * FROM postagens ORDER BY likes DESC", nativeQuery = true)
     List<Postagem> like(Integer likes);
