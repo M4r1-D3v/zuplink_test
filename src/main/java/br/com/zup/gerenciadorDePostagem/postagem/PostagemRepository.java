87d153fd91f1bc426c22beffdad80a2782d237ba
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-02-03 14:07:37 -0300

    Adicionar ordenação por likes em todos os filtros

--- src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
+++ src/main/java/br/com/zup/gerenciadorDePostagem/postagem/PostagemRepository.java
@@ -7,16 +7,20 @@ import java.util.List;
 
 public interface PostagemRepository extends CrudRepository<Postagem, Long> {
 
-    @Query(value = "SELECT * FROM postagens WHERE area_atuacao=:area", nativeQuery = true)
+    @Override
+    @Query(value = "SELECT * FROM postagens ORDER BY likes DESC", nativeQuery = true)
+    Iterable<Postagem> findAll();
+
+    @Query(value = "SELECT * FROM postagens WHERE area_atuacao=:area ORDER BY likes DESC", nativeQuery = true)
     List<Postagem> area(String area);
 
-    @Query(value = "SELECT * FROM postagens WHERE tipo=:tipo", nativeQuery = true)
+    @Query(value = "SELECT * FROM postagens WHERE tipo=:tipo ORDER BY likes DESC", nativeQuery = true)
     List<Postagem> tipo(String tipo);
 
-    @Query(value = "SELECT * FROM postagens WHERE tema=:tema", nativeQuery = true)
+    @Query(value = "SELECT * FROM postagens WHERE tema=:tema ORDER BY likes DESC", nativeQuery = true)
     List<Postagem> tema(String tema);
 
-    @Query(value = "SELECT * FROM postagens WHERE autor_postagem_id=:autorPostagem", nativeQuery = true)
+    @Query(value = "SELECT * FROM postagens WHERE autor_postagem_id=:autorPostagem ORDER BY likes DESC", nativeQuery = true)
     List<Postagem> autorPostagem(String autorPostagem);
 
     @Query(value = "SELECT * FROM postagens ORDER BY data_de_cadastro DESC", nativeQuery = true)
